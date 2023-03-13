7ded55a63a4efa99a17a2d1709e5200d68bea823
omolo-eng <billyomolo76@gmail.com>
2023-03-13 09:44:50 -0700

    first commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import add_0
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-result = add_0.add(a, b)
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-print(f"{a} + {b} = {result}\n")
 
