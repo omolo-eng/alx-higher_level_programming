a8d50c739f0d8fbdae78eda49bec19e297de1f34
omolo-eng <billyomolo76@gmail.com>
2023-04-03 06:30:56 -0700

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/test/5-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+"text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Quonam modo? Utrum tibi videor commovere litteram an totas paginas?
+Non illud modo, sed ne hoc quidem. Fortasse optimum, sed ubi illud:
+Semper plus voluptatis? Inquit, teneo finem illi nihil dolere.
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum
+oboediens rationi. Si dicis id, vicimus. Sex illa a Dipylo confecimus
+stadia vario sermone. Sin voles aliud, postea. Tribuens cuique suum
+animi affectio, quam dico. Utinam dicerent alium alio beatiorem!
+Videres iam ruinas.""")"
